a04bdae00c8019830f63ea76f7ee5a28a55e266b
subhadeepghosh456 <srsubhadeepghosh456@gmail.com>
2023-06-02 21:36:40 +0530

    first commit

---
+++ src/constant/constant.js
@@ -0,0 +1,21 @@
+export const randomColorArray = [
+    "#ff3333",
+    "#ff33bb",
+    "#33ff33",
+    "#ffff00",
+    "#ff33cc",
+    "#0099cc",
+    "#ff9966",
+    "#ff9900",
+    "#cccc00",
+    "#cc00cc",
+    "#ff6666",
+    "#33ff77",
+    "#ffcccc",
+    "#80ff80",
+    "#730099",
+    "#d24dff",
+    "#ffcc33",
+    "#99ebff",
+  ];
+  
